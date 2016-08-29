d345ac3db786f228fedaa2aca6dd017232ca51ed
Fernando Farias <fernnf@gmail.com>
2016-08-29 17:48:18 -0300

    new queries for neo4j

--- app/mtuldp-storage-app/src/main/java/br/ufpa/gercom/mtuldp/store/MtuldpStoreManager.java
+++ app/mtuldp-storage-app/src/main/java/br/ufpa/gercom/mtuldp/store/MtuldpStoreManager.java
@@ -21,12 +21,17 @@ import org.neo4j.driver.v1.Driver;
 import org.neo4j.driver.v1.GraphDatabase;
 import org.neo4j.driver.v1.Session;
 import org.neo4j.driver.v1.summary.ResultSummary;
+import org.onlab.packet.Ip4Address;
+import org.onlab.packet.IpAddress;
+import org.onlab.packet.MacAddress;
 import org.onosproject.core.ApplicationId;
 import org.onosproject.core.CoreService;
-import org.onosproject.net.ConnectPoint;
-import org.onosproject.net.Device;
-import org.onosproject.net.Link;
+import org.onosproject.net.*;
+import org.onosproject.net.behaviour.InterfaceConfig;
 import org.slf4j.Logger;
+
+import java.util.Set;
+import java.util.stream.Collectors;
 
 import static org.slf4j.LoggerFactory.getLogger;
 
@@ -59,6 +64,14 @@ public class MtuldpStoreManager {
     }
 
 
+    private enum DeviceTypes{
+        HOST, SWITCH, ROUTER;
+    }
+
+    private enum LinkTypes extends MtuldpStore {
+        DIRECT, EDGE;
+    }
+
     private class MtuldpStore {
 
         private Driver driver;
@@ -76,8 +89,11 @@ public class MtuldpStoreManager {
 
         private boolean createDevice(Device device) {
 
-            String query = String.format("CREATE (a:%s {type: '%s', device_id: '%s'})",
-                    device.type().name(), device.type().name(), device.id().toString());
+            String deviceType = DeviceTypes.SWITCH.name();
+            DeviceId deviceId = device.id();
+
+            String query = String.format("CREATE (a:%s {type: '%s', device_id: '%s'})", deviceType,
+                    deviceType, deviceId.toString());
 
             ResultSummary result = session.run(query).consume();
 
@@ -91,21 +107,59 @@ public class MtuldpStoreManager {
             ConnectPoint src = link.src();
             ConnectPoint dst = link.dst();
 
-            String query = String.format("MATCH (a {device_id:'%s'}), (b {device_id:'%s'}) " +
-                            "MERGE (a)-[r:%s{port_src: %d, port_dst: %d, mtu:'null'}]->(b)",
-                    src.deviceId().toString(), dst.deviceId().toString(), link.type().name(),
-                    Integer.getInteger(src.port().name()), Integer.getInteger(dst.port().name()));
+            String query = String.format("MATCH (a:%s {device_id:'%s'}), (b:%s {device_id:'%s'}) " +
+                            "MERGE (a)-[r:%s{port_src: %d, port_dst: %d, mtu:NULL}]->(b)",
+                    DeviceTypes.SWITCH.name(),DeviceTypes.SWITCH.name(),src.deviceId().toString(),
+                    dst.deviceId().toString(), link.type().name(), Integer.getInteger(src.port().name()),
+                    Integer.getInteger(dst.port().name()));
 
             ResultSummary result = session.run(query).consume();
 
             if (result.counters().relationshipsCreated() == 0){
                 return false;
             }
-
             return true;
         }
+
+        private boolean createHost(Host host){
+
+            String type = DeviceTypes.HOST.name();
+            HostId hostId = host.id();
+            MacAddress mac = host.mac();
+            Set<IpAddress> ipAddressSet = host.ipAddresses();
+
+            String query = String.format("CREATE(a:%s{ type:'%s',host_id:'%s', mac:'%s', ip:%s})",
+                    type,type, hostId.toString(), mac.toString(),ipAddressSet.toArray().toString());
+
+            ResultSummary result = session.run(query).consume();
+
+            if (result.counters().nodesCreated() == 0 ){
+                return false;
+            }
+            return true;
+        }
+
+        private boolean createEdgeLink(EdgeLink edgeLink){
+
+            String deviceType = DeviceTypes.SWITCH.name();
+            String edgeDeviceType = DeviceTypes.HOST.name();
+            String linkType = LinkTypes.EDGE.name();
+            DeviceId deviceId = edgeLink.hostLocation().deviceId();
+            PortNumber edgePort = edgeLink.hostLocation().port();
+            HostId hostId = edgeLink.hostId();
+
+            String query = String.format("MATCH (a:%s {host_id:'%s'}), (b:%s{device_id:'%s'}) " +
+                    "MERGE (a)-[r:'%s'{edge_port:%d, mtu: NULL} ]->(b)", edgeDeviceType, hostId.toString(),
+                    deviceType, deviceId.toString(),linkType, Integer.getInteger(edgePort.name()));
+
+            ResultSummary result = session.run(query).consume();
+
+            if (result.counters().relationshipsCreated() == 0){
+                return false;
+            }
+            return true;
+        }
+
     }
-
-
 }
 
